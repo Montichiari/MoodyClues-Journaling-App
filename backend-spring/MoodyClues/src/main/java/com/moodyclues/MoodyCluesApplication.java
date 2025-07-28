1a2c2651a9ba91e6482b316198931f68379bee41
Montichiari <124030799+Montichiari@users.noreply.github.com>
2025-07-28 16:32:02 +0800

    First commit

---
+++ backend-spring/MoodyClues/src/main/java/com/moodyclues/MoodyCluesApplication.java
@@ -0,0 +1,13 @@
+package com.moodyclues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodyCluesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodyCluesApplication.class, args);
+	}
+
+}
